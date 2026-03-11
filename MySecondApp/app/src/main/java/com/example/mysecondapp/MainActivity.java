8609564f31bf405a901c9ce7de8e3a55fd93f695
milleniumGNL <klokovgs@yandex.ru>
2026-03-11 12:27:17 +0600

    Second Lab
    Second Lab on Java.

--- MySecondApp/app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ MySecondApp/app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.firstapp;
+package com.example.mysecondapp;
 
 import android.os.Bundle;
 
@@ -8,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements BlankFragment.OnTextCopiedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    @Override
-    public void onTextCopied(String text) {
-        BlankFragment2 fragment2 = (BlankFragment2) getSupportFragmentManager()
-                .findFragmentById(R.id.main2);
-
-        if (fragment2 != null) {
-            fragment2.scaner(text);
-        }
     }
 }
--- firstApp/app/src/main/java/com/example/firstapp/BlankFragment2.java
+++ firstApp/app/src/main/java/com/example/firstapp/BlankFragment2.java
@@ -1,22 +1,21 @@
 package com.example.firstapp;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import android.widget.Button;
-import android.widget.EditText;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment#newInstance} factory method to
+ * Use the {@link BlankFragment2#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BlankFragment extends Fragment {
+public class BlankFragment2 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,17 +26,9 @@ public class BlankFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private EditText editText01;
-    //private EditText editText02;
-    private Button copyButton;
+    private EditText editText02;
 
-    public interface OnTextCopiedListener {
-        void onTextCopied(String text);
-    }
-
-    private OnTextCopiedListener listener;
-
-    public BlankFragment() {
+    public BlankFragment2() {
         // Required empty public constructor
     }
 
@@ -47,11 +38,11 @@ public class BlankFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+     * @return A new instance of fragment BlankFragment2.
      */
     // TODO: Rename and change types and number of parameters
-    public static BlankFragment newInstance(String param1, String param2) {
-        BlankFragment fragment = new BlankFragment();
+    public static BlankFragment2 newInstance(String param1, String param2) {
+        BlankFragment2 fragment = new BlankFragment2();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -72,30 +63,18 @@ public class BlankFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         assert getView() != null;
-        editText01 = getView().findViewById(R.id.editText1);
-        //editText02 = getView().findViewById(R.id.editText2);
-        copyButton = getView().findViewById(R.id.copyButton);
-
-        copyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = editText01.getText().toString();
-                //editText02.setText(text);
-                listener.onTextCopied(text);
-            }
-        });
+        editText02 = getView().findViewById(R.id.editText2);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        return inflater.inflate(R.layout.fragment_blank2, container, false);
     }
 
-    @Override
-    public void onAttach(@NonNull Context context){
-        super.onAttach(context);
-        listener = (OnTextCopiedListener) context;
+    public void scaner(String text) {
+        EditText editT = getView().findViewById(R.id.editText2);
+        editT.setText(text);
     }
 }
